aacb93cedfe5ebeae17cd51c28646f267886f138
connectbo <peterbozhou@gmail.com>
2020-10-01 11:42:50 -0400

    add api testing function

--- src/components/Authenticate.js
+++ src/components/Authenticate.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import '../App.css';
 
 function Authenticate() {
-    const [ username, setUsername ] = useState('');
-    const [ password, setPassword ] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [token, setToken] = useState('');
 
     const handleUsername = event => {
         setUsername(event.target.value);
@@ -13,25 +15,27 @@ function Authenticate() {
         setPassword(event.target.value);
     }
 
-    const handleAuthenticate = event => {
-        fetch('http://54.210.60.122:80/irods-rest/1.0.0/auth', {
+    async function handleAuthenticate() {
+        const authResult = await axios({
             method: 'POST',
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-            },
-            withCredentials: true,
-            data: {
+            url: 'http://54.210.60.122:80/irods-rest/1.0.0/auth',
+            params: {
                 userName: username,
                 password: password,
                 authType: 'STANDARD'
             }
-        })
-            .then(res => {
-                console.log(res);
-            })
-            .then(data => {
-                console.log(data);
-            })
+        }).then(res => setToken(res.data))
+    }
+
+    async function handleZoneReport(){
+        const zoneResult = await axios({
+            method: 'POST',
+            url: 'http://54.210.60.122:80/irods-rest/1.0.0/zone_report',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `${token}`
+            }
+        }).then(res => console.log(res))
     }
 
     return (
@@ -45,6 +49,7 @@ function Authenticate() {
             </div>
             <br />
             <button className="login-button" onClick={handleAuthenticate}>Login</button>
+            <button className="login-button" onClick={handleZoneReport}>ZoneReport</button>
         </div>
     );
 }
