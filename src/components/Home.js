55cf764b7ac567cb51291ae9d2d57c974844f82b
connectbo <peterbozhou@gmail.com>
2020-10-13 18:13:44 -0400

    Add Drawer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+import AppBar from '@material-ui/core/AppBar';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 function Home() {
     const [zone_report, getReport] = useState();
     const token = Cookies.get('token');
     const isAuthenticated = token != null ? true : false;
+    const theme = useTheme();
+    const drawer = (
+        <div>
+            <List>
+            {['Zone Report', 'Admin', 'Access', 'Query', 'Stream'].map((text, index) =>{
+                <ListItem button key={text}>
+                    <ListItemText primary={text} />
+                </ListItem>
+            })}
+            </List>
+        </div>
+    )
 
     useEffect(() => {
         axios({
@@ -25,6 +45,13 @@ function Home() {
         <div>
             {isAuthenticated == true ? <div>
                 You are authenticated!
+                <nav>
+                    <Drawer
+                    anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                    >
+                    {drawer}
+                    </Drawer>
+                </nav>
         </div> : <div>Please login to use the administration dashboard.</div>}
         </div>
     );
