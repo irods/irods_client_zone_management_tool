d6502dd8f3023d92f69dbb417e40a3a5f3e40f61
connectbo <peterbozhou@gmail.com>
2020-10-14 14:28:54 -0400

    Support Zone report

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,32 +3,67 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
-import AppBar from '@material-ui/core/AppBar';
 import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing(3),
+    },
+}));
+
 function Home() {
-    const [zone_report, getReport] = useState();
     const token = Cookies.get('token');
+    const [main, setMain] = useState('Welcome!')
+    const [zone_report, setReport] = useState();
     const isAuthenticated = token != null ? true : false;
+    const classes = useStyles();
     const theme = useTheme();
     const drawer = (
         <div>
             <List>
-            {['Zone Report', 'Admin', 'Access', 'Query', 'Stream'].map((text, index) =>{
-                <ListItem button key={text}>
-                    <ListItemText primary={text} />
-                </ListItem>
-            })}
+                {['Zone Report', 'Admin', 'Access', 'Query', 'Stream'].map((text, index) => {
+                    <ListItem button key={text}>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                })}
             </List>
         </div>
     )
 
-    useEffect(() => {
-        axios({
+    const logout = () => {
+        
+    }
+
+    async function get_zone_report() {
+        const result = await axios({
             method: 'POST',
             url: 'http://54.210.60.122:80/irods-rest/1.0.0/zone_report',
             headers: {
@@ -36,23 +71,53 @@ function Home() {
                 'Authorization': `${token}`
             }
         }).then(res => {
-            getReport(res);
-            console.log(res);
+            setReport(res);
+            setMain(`Schema Version: ${res.data.schema_version}`);
         })
-    }, []);
+    }
 
     return (
         <div>
-            {isAuthenticated == true ? <div>
-                You are authenticated!
-                <nav>
-                    <Drawer
-                    anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-                    >
-                    {drawer}
-                    </Drawer>
-                </nav>
-        </div> : <div>Please login to use the administration dashboard.</div>}
+            {isAuthenticated == true ? <div className={classes.root}>
+                <CssBaseline />
+                <AppBar position="fixed" className={classes.appBar}>
+                    <Toolbar>
+                        <Typography variant="h6" noWrap>
+                            iRODS Administrator Dashboard
+          </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    className={classes.drawer}
+                    variant="permanent"
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                    anchor="left"
+                >
+                    <div className={classes.toolbar} />
+                    <Divider />
+                    <List>
+                        <ListItem button key='zone_report' onClick={get_zone_report}>
+                            <ListItemText primary='Zone Report' />
+                        </ListItem>
+                    </List>
+                    <Divider />
+                    <List>
+                        {['Current Session', 'Logout'].map((text, index) => (
+                            <ListItem button key={text}>
+                                <ListItemText primary={text} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Drawer>
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+                    <div>
+                        {main}
+                    </div>
+                </main>
+            </div> : <div>Please login to use the administration dashboard.</div>}
         </div>
     );
 }
