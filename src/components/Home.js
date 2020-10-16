7ccb9966bdfb79e58bfd7c02d9d63bb7f6651d09
connectbo <peterbozhou@gmail.com>
2020-10-16 17:11:12 -0400

    Add separate sidebar component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
+import ServerIcon from '../img/servers-logo.png';
+
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import BlockIcon from '@material-ui/icons/Block'
@@ -54,7 +56,7 @@ const useStyles = makeStyles((theme) => ({
 function Home() {
     const token = Cookies.get('token');
     const [main, setMain] = useState(`Welcome, ${Cookies.get('username')}!`);
-    const [zone_report, setReport] = useState([]);
+    const [zone_reports, setReport] = useState([]);
     const isAuthenticated = token != null ? true : false;
     const classes = useStyles();
     const theme = useTheme();
@@ -87,16 +89,17 @@ function Home() {
         }).then(res => {
             let zones = res.data.zones;
             let tem_main = '';
-            setReport(res.data.zones);
-            console.log(res);
-            zones.forEach(zone => {
-                tem_main+=(`Hostname: ${zone['icat_server']['host_system_information']['hostname']}\n`);
-                tem_main+=(`OS Distribution Name: ${zone['icat_server']['host_system_information']['os_distribution_name']}\n`);
-                tem_main+=(`OS Distribution Version: ${zone['icat_server']['host_system_information']['os_distribution_version']}\n`);
-                tem_main+=(`Service Account Group Name: ${zone['icat_server']['host_system_information']['service_account_group_name']}\n`);
-                tem_main+=(`Service Account User Name: ${zone['icat_server']['host_system_information']['service_account_user_name']}\n`);
-            })
+            setReport([...res.data.zones]);
+            // zones.forEach(zone => {
+            //     tem_main+=(<img src={ServerIcon}></img>);
+            //     tem_main+=(`Hostname: ${zone[0]['icat_server']['host_system_information']['hostname']}\n`);
+            //     tem_main+=(`OS Distribution Name: ${zone['icat_server']['host_system_information']['os_distribution_name']}\n`);
+            //     tem_main+=(`OS Distribution Version: ${zone['icat_server']['host_system_information']['os_distribution_version']}\n`);
+            //     tem_main+=(`Service Account Group Name: ${zone['icat_server']['host_system_information']['service_account_group_name']}\n`);
+            //     tem_main+=(`Service Account User Name: ${zone['icat_server']['host_system_information']['service_account_user_name']}\n`);
+            // })
             setMain(tem_main);
+            console.log(zone_reports);
         })
     }
 
@@ -148,7 +151,11 @@ function Home() {
                 <main className={classes.content}>
                     <div className={classes.toolbar} />
                     <div className={classes.main}>
-                        {main}
+                        {zone_reports.map(zone_report => {
+                            <div>
+                            <img src={ServerIcon}></img>
+                            <div>Hostname: {zone_report[0]['icat_server']['host_system_information']['hostname']}</div></div>
+                        })}
                     </div>
                 </main>
             </div> : <div className={classes.logout}><BlockIcon /><br/><div>Please <a href="http://localhost:3000/">login</a> to use the administration dashboard.</div></div>}
---
+++ src/components/Sidebar.js
@@ -0,0 +1,61 @@
+import { React, useState } from 'react';
+import Cookies from 'js-cookie';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import BlockIcon from '@material-ui/icons/Block'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    logout: {
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: theme.spacing(12),
+        alignItems: 'center',
+        fontSize: 25,
+    },
+    appBar: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing(3),
+    },
+    main: {
+        whiteSpace: "pre-wrap",
+        fontSize: 20
+    }
+}));
+
+function Sidebar() {
+    const token = Cookies.get('token');
+
+    return (
+
+    );
+}
+
+export default Sidebar;
