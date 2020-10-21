a6c6b407076d0d43e9126c3a169f9efd0b484530
connectbo <peterbozhou@gmail.com>
2020-10-21 12:11:10 -0400

    Add new server display

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,11 @@ import Cookies from 'js-cookie';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 import ServerIcon from '../img/servers-logo.png';
-import BlockIcon from '@material-ui/icons/Block'
-import { CssBaseline, Typography } from '@material-ui/core';
+import BlockIcon from '@material-ui/icons/Block';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import { CssBaseline, Typography, CircularProgress } from '@material-ui/core';
+import { Avatar, Card, CardHeader, CardActions, CardContent, Collapse } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -24,16 +27,17 @@ const useStyles = makeStyles((theme) => ({
         fontSize: 20
     },
     server: {
-        width: theme.spacing(10)
+        width: theme.spacing(8)
     },
-    server_box:{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center'
+    server_card: {
+        width: theme.spacing(40),
     },
     divider: {
         marginTop: theme.spacing(20)
+    },
+    status_box: {
+        display: 'flex',
+        fontSize: theme.spacing(3),
     }
 }));
 
@@ -43,8 +47,8 @@ function Home() {
     const [zone_reports, setReport] = useState([]);
     const [result, setResult] = useState();
     const [mouseIn, setMouseIn] = useState(false);
-    const [hostname, setHostname] = useState('');
     const [currentZone, setCurrentZone] = useState({});
+    const [expanded, setExpanded] = useState(false);
     const isAuthenticated = token != null ? true : false;
     const classes = useStyles();
     const theme = useTheme();
@@ -59,6 +63,7 @@ function Home() {
                 'Authorization': `${token}`
             }
         }).then(res => {
+            console.log(res.data);
             setReport(res.data.zones);
             setResult(`Number of Server Running: ${res.data.zones.length}`);
         })
@@ -76,21 +81,45 @@ function Home() {
     }
 
     const handleMouseOut = event => {
-        setMouseIn(false);
+        setMouseIn(!mouseIn);
+    }
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
     }
 
     return (
         <div>
             {isAuthenticated == true ? <div className={classes.root}><Appbar /><Sidebar /><main className={classes.content}><div className={classes.toolbar} />
                 <div className={classes.main}>{zone_reports.length > 0 ? zone_reports.map(zone_report =>
-                    <div className={classes.server_box}>
-                        <img className={classes.server} id={zone_id++} src={ServerIcon} onMouseEnter={handleMouseIn} onMouseLeave={handleMouseOut}></img>
-                <p>iCAT Server #{zone_id}</p>
-                    </div>
-                ) : <div>Loading...</div>}</div><hr className={classes.divider}/>{mouseIn == true ? <div><p>Hostname: {currentZone['hostname']}</p><p>OS Distribution Name: {currentZone['os_distribution_version']}</p><p>OS Distribution Version: {currentZone['os_distribution_version']}</p></div> : <div>Number of Server Running: {zone_reports.length}</div>}</main>
+                    <Card className={classes.server_card}>
+                        <CardHeader
+                            avatar={
+                                <img className={classes.server} id={zone_id} src={ServerIcon} onMouseEnter={handleMouseIn} onMouseLeave={handleMouseOut}></img>
+                            }
+                            title="Server"
+                            subheader="iCAT Server"
+                            action={
+                                <IconButton
+                                    onClick={handleExpandClick}>
+                                    <ExpandMoreIcon />
+                                </IconButton>
+                            }
+                        />
+                        <Collapse in={expanded} timeout="auto" unmountOnExit>
+                            <CardContent>
+                                <Typography paragraph>Hostname: {zone_report['icat_server']['host_system_information']['hostname']}</Typography>
+                                <Typography paragraph>OS Distribution Name: {zone_report['icat_server']['host_system_information']['os_distribution_name']}</Typography>
+                                <Typography paragraph>OS Distribution Version: {zone_report['icat_server']['host_system_information']['os_distribution_version']}</Typography>
 
-            </div> : <div className={classes.logout}><BlockIcon /><br /><div>Please <a href="http://localhost:3000/">login</a> to use the administration dashboard.</div></div>}
-        </div>
+                            </CardContent>
+                        </Collapse>
+                    </Card>
+                ) : <div><CircularProgress /> Loading...</div>}</div></main>
+
+            </div> : <div className={classes.logout}><BlockIcon /><br /><div>Please <a href="http://localhost:3000/">login</a> to use the administration dashboard.</div></div>
+            }
+        </div >
     );
 }
 
