c625d4745e1cef755b0ad8ba8446f40837eab2dd
connectbo <peterbozhou@gmail.com>
2020-10-20 20:03:06 -0400

    Rebuild zone report V2

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 import ServerIcon from '../img/servers-logo.png';
 import BlockIcon from '@material-ui/icons/Block'
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -32,10 +32,11 @@ function Home() {
     const token = Cookies.get('token');
     const [main, setMain] = useState(`Welcome, ${Cookies.get('username')}!`);
     const [zone_reports, setReport] = useState([]);
-    const [test, setTest] = useState(0);
+    const [result, setResult] = useState();
     const isAuthenticated = token != null ? true : false;
     const classes = useStyles();
     const theme = useTheme();
+    let zone_id = 0;
 
     useEffect(() => {
         const result = axios({
@@ -47,33 +48,20 @@ function Home() {
             }
         }).then(res => {
             setReport(res.data.zones);
-            setTest(1);
+            setResult(`Number of Server Running: ${res.data.zones.length}`);
         })
     }, [isAuthenticated]);
 
-    async function get_zone_report() {
-        const result = await axios({
-            method: 'POST',
-            url: 'http://54.210.60.122:80/irods-rest/1.0.0/zone_report',
-            headers: {
-                'Accept': 'application/json',
-                'Authorization': `${token}`
-            }
-        }).then(res => {
-            let zones = res.data.zones;
-            let tem_main = '';
-            setReport([...res.data.zones]);
-            // zones.forEach(zone => {
-            //     tem_main+=(<img src={ServerIcon}></img>);
-            //     tem_main+=(`Hostname: ${zone[0]['icat_server']['host_system_information']['hostname']}\n`);
-            //     tem_main+=(`OS Distribution Name: ${zone['icat_server']['host_system_information']['os_distribution_name']}\n`);
-            //     tem_main+=(`OS Distribution Version: ${zone['icat_server']['host_system_information']['os_distribution_version']}\n`);
-            //     tem_main+=(`Service Account Group Name: ${zone['icat_server']['host_system_information']['service_account_group_name']}\n`);
-            //     tem_main+=(`Service Account User Name: ${zone['icat_server']['host_system_information']['service_account_user_name']}\n`);
-            // })
-            setMain(tem_main);
-            console.log(zone_reports);
-        })
+
+    const mouseIn = event => {
+        const current_zone = event.target.id;
+        let tem_result = '';
+        tem_result += (`Hostname: ${zone_reports[current_zone]['icat_server']['host_system_information']['hostname']}\n`);
+        tem_result += (`OS Distribution Name: ${zone_reports[current_zone]['icat_server']['host_system_information']['os_distribution_name']}\n`);
+        tem_result += (`OS Distribution Version: ${zone_reports[current_zone]['icat_server']['host_system_information']['os_distribution_version']}\n`);
+        tem_result += (`Service Account Group Name: ${zone_reports[current_zone]['icat_server']['host_system_information']['service_account_group_name']}\n`);
+        tem_result += (`Service Account User Name: ${zone_reports[current_zone]['icat_server']['host_system_information']['service_account_user_name']}\n`);
+        setResult(tem_result);
     }
 
     return (
@@ -81,21 +69,11 @@ function Home() {
             {isAuthenticated == true ? <div className={classes.root}><Appbar /><Sidebar /><main className={classes.content}><div className={classes.toolbar} />
                 <div className={classes.main}>{zone_reports.length > 0 ? zone_reports.map(zone_report =>
                     <div>
-                        <img className={classes.server} src={ServerIcon}></img>
-                        <p>Hostname: {zone_report['icat_server']['host_system_information']['hostname']}</p>
+                        <img className={classes.server} id={zone_id++} src={ServerIcon} onMouseEnter={mouseIn}></img>
+                        <p>iCAT Server</p>
                     </div>
-                ) : <div>Loading...</div>}</div></main>
-                {/* <main className={classes.content}>
-                    <div className={classes.toolbar} />
-                    <div className={classes.main}>
-                        <div>{test}</div>
-                        {zone_reports.map(zone_report => {
-                            <div>
-                                <img src={ServerIcon}></img>
-                                <div>Hostname: {zone_report['icat_server']['host_system_information']['hostname']}</div></div>
-                        })}
-                    </div>
-                </main> */}
+                ) : <div>Loading...</div>}</div><hr />{zone_reports.length > 0 ? <div>{result}</div> : <br />}</main>
+
             </div> : <div className={classes.logout}><BlockIcon /><br /><div>Please <a href="http://localhost:3000/">login</a> to use the administration dashboard.</div></div>}
         </div>
     );
