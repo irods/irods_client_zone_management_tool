ab40412096409a0606c83fed5c3c71ec223ce15f
connectbo <peterbozhou@gmail.com>
2020-10-27 14:57:01 -0400

    Add Zone details

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,8 @@ import IconButton from '@material-ui/core/IconButton';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import Pagination from '@material-ui/lab/Pagination';
 import { CssBaseline, Typography, CircularProgress } from '@material-ui/core';
-import { Avatar, Card, CardHeader, CardActions, CardContent, Collapse } from '@material-ui/core';
+import { Avatar, Button, Card, CardHeader, CardActions, CardContent, Collapse } from '@material-ui/core';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -51,11 +52,9 @@ const useStyles = makeStyles((theme) => ({
 
 function Home() {
     const token = Cookies.get('token');
-    const [main, setMain] = useState(`Welcome, ${Cookies.get('username')}!`);
     const [zone_reports, setReport] = useState([]);
-    const [result, setResult] = useState();
-    const [mouseIn, setMouseIn] = useState(false);
-    const [currentZone, setCurrentZone] = useState({});
+    const [curr_zone, setCurrZone] = useState();
+    const [details, setDetails] = useState(false);
     const [expanded, setExpanded] = useState(false);
     const isAuthenticated = token != null ? true : false;
     const classes = useStyles();
@@ -73,7 +72,7 @@ function Home() {
         }).then(res => {
             console.log(res);
             setReport(res.data.zones);
-            setResult(`Number of Server Running: ${res.data.zones.length}`);
+            Cookies.set('zone_name', res.data.zones[0]['icat_server']['service_account_environment']['irods_zone_name'])
         })
     }, [isAuthenticated]);
 
@@ -81,11 +80,21 @@ function Home() {
         setExpanded(!expanded);
     }
 
+    const viewDetails = event => {
+        setCurrZone(zone_reports[document.getElementsByClassName(classes.server_card)[0].id]['icat_server']);
+        console.log(curr_zone);
+        setDetails(true);
+    }
+
+    const closeDetails = event => {
+        setDetails(false);
+    }
+
     return (
         <div>
             {isAuthenticated == true ? <div className={classes.root}><Appbar /><Sidebar /><main className={classes.content}><div className={classes.toolbar} />
-    <div className={classes.main}><Typography>Number of Server Running: {zone_reports.length}</Typography><br/><Pagination count={1} /><br/>{zone_reports.length > 0 ? zone_reports.map(zone_report =>
-                    <Card className={classes.server_card}>
+                <div className={classes.main}><Typography>Number of Server Running: {zone_reports.length}</Typography><br /><Pagination count={1} /><br />{zone_reports.length > 0 ? zone_reports.map(zone_report =>
+                    <Card className={classes.server_card} id={zone_id}>
                         <CardHeader
                             avatar={
                                 <img className={classes.server} id={zone_id} src={ServerIcon}></img>
@@ -104,8 +113,15 @@ function Home() {
                                 <Typography paragraph>Hostname: {zone_report['icat_server']['host_system_information']['hostname']}</Typography>
                                 <Typography paragraph>OS Distribution Name: {zone_report['icat_server']['host_system_information']['os_distribution_name']}</Typography>
                                 <Typography paragraph>OS Distribution Version: {zone_report['icat_server']['host_system_information']['os_distribution_version']}</Typography>
-
                             </CardContent>
+                            <Button onClick={viewDetails}>Details</Button>
+                            {details == true ? <Dialog open={details} onClose={closeDetails}>
+                                <DialogTitle>Server Details</DialogTitle>
+                                <DialogContent>
+                                    <Typography>Schema Name: {curr_zone['host_access_control_config']['schema_name']}</Typography>
+                                    <Typography>Schema Version: {curr_zone['host_access_control_config']['schema_version']}</Typography>
+                                </DialogContent>
+                            </Dialog> : <span />}
                         </Collapse>
                     </Card>
                 ) : <div><CircularProgress /> Loading...</div>}</div></main>
@@ -115,7 +131,5 @@ function Home() {
         </div >
     );
 }
-
-// 50 iRODS/ 25 helx UI/ 25 robokop UI 
 
 export default Home;
