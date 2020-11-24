dff7efe11ac39db7721a3484ddbde9aa696eb46b
connectbo <peterbozhou@gmail.com>
2020-11-24 17:49:36 -0500

    Refine Row Component

--- src/components/Rows.js
+++ src/components/Rows.js
@@ -1,13 +1,21 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
 
 function Rows(props){
     const { row } = props;
     const [open, setOpen] = React.useState(false);
-    const classes = useRowStyles();
   
     return (
       <React.Fragment>
-        <TableRow className={classes.root}>
+        <TableRow>
           <TableCell>
             <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
               {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
@@ -16,10 +24,10 @@ function Rows(props){
           <TableCell component="th" scope="row">
             {row.name}
           </TableCell>
-          <TableCell align="right">{row.calories}</TableCell>
-          <TableCell align="right">{row.fat}</TableCell>
-          <TableCell align="right">{row.carbs}</TableCell>
-          <TableCell align="right">{row.protein}</TableCell>
+          <TableCell align="right">{row[0]}</TableCell>
+          <TableCell align="right">{row[1]}</TableCell>
+          <TableCell align="right">{row[2]}</TableCell>
+          <TableCell align="right"></TableCell>
         </TableRow>
         <TableRow>
           <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
@@ -37,20 +45,6 @@ function Rows(props){
                       <TableCell align="right">Total price ($)</TableCell>
                     </TableRow>
                   </TableHead>
-                  <TableBody>
-                    {row.history.map((historyRow) => (
-                      <TableRow key={historyRow.date}>
-                        <TableCell component="th" scope="row">
-                          {historyRow.date}
-                        </TableCell>
-                        <TableCell>{historyRow.customerId}</TableCell>
-                        <TableCell align="right">{historyRow.amount}</TableCell>
-                        <TableCell align="right">
-                          {Math.round(historyRow.amount * row.price * 100) / 100}
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
                 </Table>
               </Box>
             </Collapse>
