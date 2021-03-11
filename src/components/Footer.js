5c252ecc10c707e5ff4b4ab75c9371635daf6e9e
connectbo <peterbozhou@gmail.com>
2021-03-11 16:11:47 -0500

    [#19] create footer component and clean authenticate page

---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
+import logo from '../img/iRODS-logo.png';
+import { version } from '../../package.json';
+import { useConnection } from '../contexts/ConnectionContext';
+import { useEnvironment } from '../contexts/EnvironmentContext';
+
+function Footer() {
+    const [connectionForm, setConnectionForm] = useState(false);
+    const connection = useConnection();
+    const environment = useEnvironment();
+
+    return (
+        <div className="app_footer">
+            <Dialog open={connectionForm} onClose={() => setConnectionForm(false)}>
+                <DialogTitle>
+                    Endpoint Connection
+                    </DialogTitle>
+                <DialogContent>
+                    <div className="connection_container">
+                        <div className="connection_text_status">/admin: {connection.adminConnection === undefined ? <span>Loading</span> : <span>{connection.adminConnection ? <span><div className="green_dot" />OK</span> : <span><div className="red_dot"></div>Error</span>}</span>}</div>
+                        <div className="connection_text_status">/auth: {connection.authConnection === undefined ? <span>Loading</span> : <span>{connection.authConnection ? <span><div className="green_dot" />OK</span> : <span><div className="red_dot"></div>Error</span>}</span>}</div>
+                        <div className="connection_text_status">/query: {connection.queryConnection === undefined ? <span>Loading</span> : <span>{connection.queryConnection ? <span><div className="green_dot" />OK</span> : <span><div className="red_dot"></div>Error</span>}</span>}</div>
+                        <div className="connection_text_status">/zone_report: {connection.zoneReportConnection === undefined ? <span>Loading</span> : <span>{connection.zoneReportConnection ? <span><div className="green_dot" />OK</span> : <span><div className="red_dot"></div>Error</span>}</span>}</div>
+                    </div>
+                    <div className="connection_timestamp">Last checked: <br />{connection.timeStamp}</div>
+                    <div className="connection_timestamp">
+                        <Button variant="outlined" fullWidth="true" color="primary" onClick={() => connection.testConnection()}>Test Connection</Button>
+                    </div>
+                </DialogContent>
+            </Dialog>
+            <div>{connection.connection ? <div className="green_dot"></div> : <div className="red_dot"></div>} <a className="pointer" onClick={() => setConnectionForm(true)}>iRODS Client REST API Connection</a></div>
+            <div><img className="app_footer_logo" src={logo}></img><a href="https://irods.org" className="app_footer_text">iRODS Consortium © 2021</a></div>
+            <div>Zone Management Tool Version: {version}, {process.env.REACT_APP_GIT_SHA.substring(0, 7)}</div>
+        </div>
+    )
+}
+
+export default Footer;
--- src/views/Authenticate.js
+++ src/views/Authenticate.js
@@ -7,7 +7,6 @@ import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
 
 import '../App.css';
 
@@ -41,12 +40,6 @@ function Authenticate() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [incorrect, setIncorrect] = useState(false);
-    const [connection, setConnection] = useState(true);
-    const [authConnection, setAuthConnection] = useState(true);
-    const [zoneReportConnection, setZoneReportConnection] = useState(true);
-    const [adminConnection, setAdminConnection] = useState(true);
-    const [queryConnection, setQueryConnection] = useState(true);
-    const [connectionForm, setConnectionForm] = useState(false);
     const classes = useStyles();
     const navigate = useNavigate();
     const server = useServer();
@@ -57,69 +50,6 @@ function Authenticate() {
         navigate('/home', { replace: true });
     }
 
-    // test each endpoint connection, show error if a 500 status code is returned
-
-    const testAuthConnection = async () => {
-        await axios({
-            url: `${environment.restApiLocation}/irods-rest/1.0.0/auth`,
-            method: 'POST',
-            params: {
-                user_name: 'test',
-                password: 'test',
-                auth_type: 'native'
-            }
-        }).catch(e => {
-            if (e.response.status >= 500) {
-                setAuthConnection(false);
-                setConnection(false);
-            }
-        })
-    }
-
-    const testZoneReportConnection = async () => {
-        await axios({
-            url: `${environment.restApiLocation}/irods-rest/1.0.0/zone_report`,
-            method: 'POST'
-        }).catch(e => {
-            if (e.response.status >= 500) {
-                setZoneReportConnection(false);
-                setConnection(false);
-            }
-        })
-    }
-
-    const testAdminConnection = async () => {
-        await axios({
-            url: `${environment.restApiLocation}/irods-rest/1.0.0/admin`,
-            method: 'POST'
-        }).catch(e => {
-            if (e.response.status >= 500) {
-                setAdminConnection(false);
-                setConnection(false);
-            }
-        })
-    }
-
-    const testQueryConnection = async () => {
-        await axios({
-            url: `${environment.restApiLocation}/irods-rest/1.0.0/query`,
-            method: 'GET'
-        }).catch(e => {
-            if (e.response.status >= 500) {
-                setQueryConnection(false);
-                setConnection(false);
-            }
-        })
-    }
-
-    // check each endpoint per render
-    useEffect(() => {
-        testAuthConnection();
-        testZoneReportConnection();
-        testAdminConnection();
-        testQueryConnection();
-    }, [])
-    
     const handleUsername = (event) => {
         setUsername(event.target.value);
     }
@@ -189,21 +119,8 @@ function Authenticate() {
                     className={classes.login_button}
                     onClick={handleAuthenticate}>Login</Button>
                 <br />
-                <Dialog open={connectionForm} onClose={() => setConnectionForm(false)}>
-                    <DialogTitle>
-                        Endpoint Connection
-                    </DialogTitle>
-                    <DialogContent>
-                        <div className="connection_container">
-                            <div>/auth: {authConnection ? <div className="green_dot"></div> : <div className="red_dot"></div>}<Button variant="outlined" color="primary" onClick={testAuthConnection}>Retry</Button></div>
-                            <div>/zone_report: {zoneReportConnection ? <div className="green_dot"></div> : <div className="red_dot"></div>}<Button variant="outlined" color="primary" onClick={testZoneReportConnection}>Retry</Button></div>
-                            <div>/admin: {adminConnection ? <div className="green_dot"></div> : <div className="red_dot"></div>}<Button variant="outlined" color="primary" onClick={testAdminConnection}>Retry</Button></div>
-                            <div>/query: {queryConnection ? <div className="green_dot"></div> : <div className="red_dot"></div>}<Button variant="outlined" color="primary" onClick={testQueryConnection}>Retry</Button></div>
-                        </div>
-                    </DialogContent>
-                </Dialog>
                 <Box mt={8}>
-                    {connection ? <div className="green_dot"></div> : <div className="red_dot"></div>} <a className="pointer" onClick={() => setConnectionForm(true)}>iRODS Client REST API Connection</a>
+                    Copyright © iRODS Consortium 2021
                 </Box>
             </div>
         </Container>
