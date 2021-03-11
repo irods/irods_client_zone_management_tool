f46d7a263e775787f6b5befc000035810313a073
connectbo <peterbozhou@gmail.com>
2021-03-11 16:12:56 -0500

    [#19] load connection timeout variable

--- src/contexts/EnvironmentContext.js
+++ src/contexts/EnvironmentContext.js
@@ -1,16 +1,20 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const EnvironmentContext = createContext();
 
 export const EnvironmentProvider = ({ children }) => {
+    console.log("EnvironmentProvider")
+    const [timestamp, setTimestamp] = useState(new Date().toString());
 
     return (
         <EnvironmentContext.Provider value={{
             restApiLocation: process.env.REACT_APP_REST_API_URL,
+            restApiTimeout: process.env.REACT_APP_REST_API_CONNECTION_TIMEOUT_SECOND,
             primaryColor: process.env.REACT_APP_PRIMARY_COLOR,
             appbarLogo: process.env.REACT_APP_APPBAR_LOGO,
             loginLogo: process.env.REACT_APP_LOGIN_LOGO,
-            brandingName: process.env.REACT_APP_BRANDING_NAME
+            brandingName: process.env.REACT_APP_BRANDING_NAME,
         }}>
             {children}
         </EnvironmentContext.Provider>
