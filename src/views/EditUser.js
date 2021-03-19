8b2b5b38faa78efaed9a54378a2de65448f85a82
connectbo <peterbozhou@gmail.com>
2021-03-19 16:13:39 -0400

    [#24] refactor edit user page

--- src/views/EditUser.js
+++ src/views/EditUser.js
@@ -15,6 +15,7 @@ import { Button, FormControl, LinearProgress, TextField, Typography } from '@mat
 import { makeStyles } from '@material-ui/core';
 import { Table, TableBody, TableCell, TableHead, TableRow, Paper } from '@material-ui/core';
 import { useEnvironment } from '../contexts/EnvironmentContext';
+import { useServer } from '../contexts/ServerContext';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -40,6 +41,12 @@ const useStyles = makeStyles((theme) => ({
     },
     link_button: {
         textDecoration: "none"
+    },
+    search_textfield: {
+        marginLeft: '2vw'
+    },
+    add_button: {
+        color: '#04bdaf'
     }
 }));
 
@@ -53,33 +60,29 @@ function EditUser(props) {
     const classes = useStyles();
     const [isLoading, setLoading] = useState(false);
     const [refresh, setRefresh] = useState(false);
-    const environment = useEnvironment();
-
+    const { restApiLocation, auth } = useEnvironment();
+    const { zoneName } = useServer();
     const [groupsOfUser, setGroupOfUser] = useState([]);
-    const [searchGroupName, setSearchName] = useState();
+    const [searchGroupName, setSearchName] = useState('');
     const [searchGroupNameResult, setSearchNameResult] = useState([]);
 
     useEffect(() => {
         setLoading(true);
         const result = axios({
             method: 'GET',
-            url: `${environment.restApiLocation}/irods-rest/1.0.0/query`,
+            url: `${restApiLocation}/irods-rest/1.0.0/query`,
             headers: {
                 'Accept': 'application/json',
-                'Authorization': token
+                'Authorization': auth
             },
             params: {
-                query_string: `SELECT USER_NAME, USER_ZONE WHERE USER_NAME = '${currentUser[0]}' AND USER_TYPE = 'rodsgroup'`,
+                query_string: `SELECT USER_GROUP_NAME WHERE USER_NAME = '${currentUser[0]}' AND USER_GROUP_NAME != '${currentUser[0]}'`,
                 query_limit: 100,
                 row_offset: 0,
                 query_type: 'general'
             }
         }).then((res) => {
-            let newArray = [];
-            for (let user in res.data._embedded) {
-                if (res.data._embedded[user][0] !== currentUser[0]) newArray.push(res.data._embedded[user]);
-            }
-            setGroupOfUser(newArray);
+            setGroupOfUser(res.data._embedded);
             setLoading(false);
         })
     }, [refresh])
@@ -87,13 +90,13 @@ function EditUser(props) {
     useEffect(() => {
         const searchResult = axios({
             method: 'GET',
-            url: `${environment.restApiLocation}/irods-rest/1.0.0/query`,
+            url: `${restApiLocation}/irods-rest/1.0.0/query`,
             headers: {
-                'Authorization': token,
+                'Authorization': auth,
             },
             params: {
                 query_string: `SELECT USER_NAME WHERE USER_GROUP_NAME LIKE '%${searchGroupName}%' AND USER_TYPE = 'rodsgroup'`,
-                query_limit: 3,
+                query_limit: 100,
                 row_offset: 0,
                 query_type: 'general'
             }
@@ -106,17 +109,17 @@ function EditUser(props) {
         try {
             const removeUserResult = await axios({
                 method: 'POST',
-                url: `${environment.restApiLocation}/irods-rest/1.0.0/admin`,
+                url: `${restApiLocation}/irods-rest/1.0.0/admin`,
                 params: {
                     action: 'modify',
                     target: 'group',
                     arg2: props[0],
                     arg3: 'remove',
                     arg4: currentUser[0],
-                    arg5: currentUser[2]
+                    arg5: zoneName
                 },
                 headers: {
-                    'Authorization': token,
+                    'Authorization': auth,
                     'Accept': 'application/json'
                 }
             }).then((res) => {
@@ -132,17 +135,17 @@ function EditUser(props) {
         try {
             await axios({
                 method: 'POST',
-                url: `${environment.restApiLocation}/irods-rest/1.0.0/admin`,
+                url: `${restApiLocation}/irods-rest/1.0.0/admin`,
                 params: {
                     action: 'modify',
                     target: 'group',
                     arg2: props[0],
                     arg3: 'add',
                     arg4: currentUser[0],
-                    arg5: currentUser[2]
+                    arg5: zoneName
                 },
                 headers: {
-                    'Authorization': token,
+                    'Authorization': auth,
                     'Accept': 'application/json'
                 }
             }).then((res) => {
@@ -154,65 +157,55 @@ function EditUser(props) {
         }
     }
 
-    const handleSearchGroupName = (event) => {
-        setSearchName(event.target.value);
+    const checkGroup = (group) => {
+        for (let i = 0; i < groupsOfUser.length; i++) {
+            if (groupsOfUser[i][0] == group[0]) {
+                return true;
+            }
+        }
+        return false;
     }
 
     return (
         <div className={classes.root}>
             <Appbar />
-            <Sidebar menu_id="1"/>
+            <Sidebar menu_id="1" />
             <main className={classes.content}>
                 <div className={classes.toolbar} />
                 <div className={classes.main}>
                     <Link to="/user" className={classes.link_button}><Button><ArrowBackIcon /></Button></Link>
                     {currentUser[0]}
-                    <div>
-                        <Table className={classes.user_table} aria-label="simple table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell><b>Group Name</b></TableCell>
-                                    <TableCell align="right"><b>Zone</b></TableCell>
-                                    <TableCell align="right"><b>Action</b></TableCell>
-                                </TableRow>
-                            </TableHead>
-                            {groupsOfUser.length > 0 ? <TableBody>
-                                {groupsOfUser.map((thisGroup) => <TableRow>
-                                    <TableCell component="th" scope="row">{thisGroup[0]}</TableCell>
-                                    <TableCell align="right">{thisGroup[1]}</TableCell>
-                                    <TableCell align='right'><Button color="secondary" onClick={() => { removeGroupFromUser(thisGroup) }}>Remove</Button></TableCell>
-                                </TableRow>)}
-                            </TableBody> : <span />}
-                        </Table>
-                        <br />
-                        <FormControl className={classes.formControl}>
-                            <Typography>Add Groups: </Typography>
-                            <TextField
-                                native
-                                id="searchGroupName"
-                                label="Group Name"
-                                onChange={handleSearchGroupName}
-                            />
-                        </FormControl>
+                    <div className="edit_search_bar">
+                        <Typography>Find Group</Typography>
+                        <TextField
+                            native
+                            id="searchGroupName"
+                            label="Search Groupname"
+                            className={classes.search_textfield}
+                            onChange={() => setSearchName(event.target.value)}
+                        />
+                    </div>
+                    <br />
+                    <div className="edit_container">
                         {searchGroupNameResult.length > 0 ?
                             <Table className={classes.user_table} aria-label="simple table">
                                 <TableHead>
                                     <TableRow>
                                         <TableCell><b>Group Name</b></TableCell>
-                                        <TableCell align="right"><b>Zone</b></TableCell>
+                                        <TableCell align="right"><b>Status</b></TableCell>
                                         <TableCell align="right"><b>Action</b></TableCell>
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
                                     {searchGroupNameResult.map((thisGroup) => <TableRow>
                                         <TableCell component="th" scope="row">{thisGroup[0]}</TableCell>
-                                        <TableCell align="right">{thisGroup[1]}</TableCell>
-                                        <TableCell align='right'><Button color="secondary" onClick={() => { addGroupToUser(thisGroup) }}>Add</Button></TableCell>
+                                        <TableCell align="right">{checkGroup(thisGroup) ? "In group" : "Not in group"}</TableCell>
+                                        <TableCell align='right'>{checkGroup(thisGroup) ? <Button color="secondary" onClick={() => { removeGroupFromUser(thisGroup) }}>Remove</Button> : <Button className={classes.add_button} onClick={() => { addGroupToUser(thisGroup) }}>Add</Button>}</TableCell>
                                     </TableRow>)}
                                 </TableBody>
                             </Table> : <br />}
-                        {isLoading == true ? <div><LinearProgress /></div> : <div />}
                     </div>
+                    {isLoading == true ? <div><LinearProgress /></div> : <div />}
                 </div>
             </main>
         </div>
