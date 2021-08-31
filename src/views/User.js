c0b9d2481eac3baabd7d2969ba8ee03728d06ab7
connectbo <zbo@renci.org>
2021-08-31 16:39:12 -0400

    [#57] Add more error handling when rest api is down

--- src/views/User.js
+++ src/views/User.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState, useEffect } from 'react';
 import { Link } from '@reach/router';
 import axios from 'axios';
 import { useEnvironment, useServer } from '../contexts';
-import { makeStyles, StylesProvider, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, Typography, Input, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination, TableSortLabel, Select, Paper } from '@material-ui/core';
+import { makeStyles, StylesProvider, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, LinearProgress, TextField, Typography, Input, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination, TableSortLabel, Select, Paper } from '@material-ui/core';
 import SaveIcon from '@material-ui/icons/Save';
 import CloseIcon from '@material-ui/icons/Close';
 import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
@@ -61,8 +61,7 @@ export const User = () => {
     const [currPage, setCurrPage] = useState(1);
     const [perPage, setPerPage] = useState(10);
     const [filterUsername, setFilterName] = useState('');
-    const { zoneName, userContext, loadUser } = useServer();
-
+    const { isLoadingUserContext, userContext, zoneName, loadUser } = useServer();
     const [order, setOrder] = useState("asc");
     const [orderBy, setOrderBy] = useState("USER_NAME");
 
@@ -157,75 +156,80 @@ export const User = () => {
 
     return (
         <Fragment>
-                <div className={classes.filterGroup}>
-                    <TextField
-                        className={classes.filter}
-                        id="filter-term"
-                        label="Filter"
-                        placeholder="Filter by UserName"
-                        onChange={(event) => setFilterName(event.target.value)}
-                    />
-                    <Button className={classes.add_button} variant="outlined" color="primary" onClick={handleAddRowOpen}>
-                        Add New User
-                    </Button>
-                </div>
-                <TablePagination component="div" className={classes.pagination} page={currPage - 1} count={parseInt(userContext.total)} rowsPerPage={perPage} onChangePage={handlePageChange} onChangeRowsPerPage={(e) => { setPerPage(e.target.value); setCurrPage(1) }} />
-                <TableContainer className={classes.tableContainer} component={Paper}>
-                    <Table className={classes.table} aria-label="simple table">
-                        <TableHead>
-                            <StylesProvider injectFirst>
-                                <TableRow>
-                                    <TableCell style={{ fontSize: '1.1rem', width: '20%' }}><b>Username</b><TableSortLabel active={orderBy === 'USER_NAME'} direction={orderBy === 'USER_NAME' ? order : 'asc'} onClick={() => { handleSort('USER_NAME') }} /></TableCell>
-                                    <TableCell style={{ fontSize: '1.1rem', width: '20%' }} align="right"><b>Type</b><TableSortLabel active={orderBy === 'USER_TYPE'} direction={orderBy === 'USER_TYPE' ? order : 'asc'} onClick={() => { handleSort('USER_TYPE') }} /></TableCell>
-                                    <TableCell style={{ fontSize: '1.1rem', width: '20%' }} align="right"><b>Action</b></TableCell>
-                                </TableRow>
-                            </StylesProvider>
-                        </TableHead>
-                        <TableBody>
-                            <TableRow id="add-user-row" style={{ display: 'none' }}>
-                                <TableCell><Input id="add-user-name" placeholder="Enter new username" /></TableCell>
-                                <TableCell align="right"><Select
-                                    native
-                                    id="add-user-type"
-                                >
-                                    {userTypes.map(this_user_type => <option key={this_user_type} value={this_user_type}>{this_user_type}</option>)}
-                                </Select></TableCell>
-                                <TableCell align="right"><ToggleButtonGroup size="small"><ToggleButton value="save" onClick={addUser}><SaveIcon /></ToggleButton><ToggleButton value="close" onClick={handleAddRowClose}><CloseIcon /></ToggleButton></ToggleButtonGroup></TableCell>
-                            </TableRow>
-                            {userContext !== undefined ? userContext._embedded.map((this_user) =>
-                                <TableRow key={this_user[0]}>
-                                    <TableCell style={{ fontSize: '1.1rem', width: '20%' }} component="th" scope="row">{this_user[0]}</TableCell>
-                                    <TableCell style={{ fontSize: '1.1rem', width: '20%' }} align="right">{this_user[1]}</TableCell>
-                                    <TableCell style={{ fontSize: '1.1rem', width: '20%' }} align="right"> {(this_user[0] === 'rods' || this_user[0] === 'public') ? <p></p> : <span><Link className={classes.link_button} to='/users/edit' state={{ userInfo: this_user }}><Button color="primary">Edit</Button></Link>
-                                        <Button color="secondary" onClick={() => { handleRemoveConfirmationOpen(this_user) }}>Remove</Button></span>}</TableCell>
-                                </TableRow>
-                            ) : <span />}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <Dialog open={addFormOpen} onClose={handleAddFormClose} aria-labelledby="form-dialog-title">
-                    <DialogTitle>Adding New User</DialogTitle>
-                    <DialogContent>
-                        <DialogContentText>
-                            Error Message:
-                        </DialogContentText>
-                        <p className={classes.errorMsg}>{addErrorMsg}</p>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={handleAddFormClose} color="primary">Close</Button>
-                    </DialogActions>
-                </Dialog>
-                <Dialog open={removeConfirmation} onClose={handleRemoveConfirmationClose} aria-labelledby="form-dialog-title">
-                    <DialogTitle>Warning</DialogTitle>
-                    <DialogContent>
-                        <Typography>Are you sure to remove <b>{currUser[0]}</b>?</Typography>
-                        <p className={classes.errorMsg}>{removeErrorMsg}</p>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={removeUser} color="secondary">Remove</Button>
-                        <Button onClick={handleRemoveConfirmationClose} color="primary">Cancel</Button>
-                    </DialogActions>
-                </Dialog>
+            {isLoadingUserContext ? <LinearProgress /> :
+                (userContext === undefined ? <div>Cannot load user data. Please check your iRODS Client REST API endpoint connection.</div> :
+                    <Fragment>
+                        <div className={classes.filterGroup}>
+                            <TextField
+                                className={classes.filter}
+                                id="filter-term"
+                                label="Filter"
+                                placeholder="Filter by UserName"
+                                onChange={(event) => setFilterName(event.target.value)}
+                            />
+                            <Button className={classes.add_button} variant="outlined" color="primary" onClick={handleAddRowOpen}>
+                                Add New User
+                            </Button>
+                        </div>
+                        <TablePagination component="div" className={classes.pagination} page={currPage - 1} count={parseInt(userContext.total)} rowsPerPage={perPage} onChangePage={handlePageChange} onChangeRowsPerPage={(e) => { setPerPage(e.target.value); setCurrPage(1) }} />
+                        <TableContainer className={classes.tableContainer} component={Paper}>
+                            <Table className={classes.table} aria-label="simple table">
+                                <TableHead>
+                                    <StylesProvider injectFirst>
+                                        <TableRow>
+                                            <TableCell style={{ fontSize: '1.1rem', width: '20%' }}><b>Username</b><TableSortLabel active={orderBy === 'USER_NAME'} direction={orderBy === 'USER_NAME' ? order : 'asc'} onClick={() => { handleSort('USER_NAME') }} /></TableCell>
+                                            <TableCell style={{ fontSize: '1.1rem', width: '20%' }} align="right"><b>Type</b><TableSortLabel active={orderBy === 'USER_TYPE'} direction={orderBy === 'USER_TYPE' ? order : 'asc'} onClick={() => { handleSort('USER_TYPE') }} /></TableCell>
+                                            <TableCell style={{ fontSize: '1.1rem', width: '20%' }} align="right"><b>Action</b></TableCell>
+                                        </TableRow>
+                                    </StylesProvider>
+                                </TableHead>
+                                <TableBody>
+                                    <TableRow id="add-user-row" style={{ display: 'none' }}>
+                                        <TableCell><Input id="add-user-name" placeholder="Enter new username" /></TableCell>
+                                        <TableCell align="right"><Select
+                                            native
+                                            id="add-user-type"
+                                        >
+                                            {userTypes.map(this_user_type => <option key={this_user_type} value={this_user_type}>{this_user_type}</option>)}
+                                        </Select></TableCell>
+                                        <TableCell align="right"><ToggleButtonGroup size="small"><ToggleButton value="save" onClick={addUser}><SaveIcon /></ToggleButton><ToggleButton value="close" onClick={handleAddRowClose}><CloseIcon /></ToggleButton></ToggleButtonGroup></TableCell>
+                                    </TableRow>
+                                    {userContext !== undefined ? userContext._embedded.map((this_user) =>
+                                        <TableRow key={this_user[0]}>
+                                            <TableCell style={{ fontSize: '1.1rem', width: '20%' }} component="th" scope="row">{this_user[0]}</TableCell>
+                                            <TableCell style={{ fontSize: '1.1rem', width: '20%' }} align="right">{this_user[1]}</TableCell>
+                                            <TableCell style={{ fontSize: '1.1rem', width: '20%' }} align="right"> {(this_user[0] === 'rods' || this_user[0] === 'public') ? <p></p> : <span><Link className={classes.link_button} to='/users/edit' state={{ userInfo: this_user }}><Button color="primary">Edit</Button></Link>
+                                                <Button color="secondary" onClick={() => { handleRemoveConfirmationOpen(this_user) }}>Remove</Button></span>}</TableCell>
+                                        </TableRow>
+                                    ) : <span />}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                        <Dialog open={addFormOpen} onClose={handleAddFormClose} aria-labelledby="form-dialog-title">
+                            <DialogTitle>Adding New User</DialogTitle>
+                            <DialogContent>
+                                <DialogContentText>
+                                    Error Message:
+                                </DialogContentText>
+                                <p className={classes.errorMsg}>{addErrorMsg}</p>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={handleAddFormClose} color="primary">Close</Button>
+                            </DialogActions>
+                        </Dialog>
+                        <Dialog open={removeConfirmation} onClose={handleRemoveConfirmationClose} aria-labelledby="form-dialog-title">
+                            <DialogTitle>Warning</DialogTitle>
+                            <DialogContent>
+                                <Typography>Are you sure to remove <b>{currUser[0]}</b>?</Typography>
+                                <p className={classes.errorMsg}>{removeErrorMsg}</p>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={removeUser} color="secondary">Remove</Button>
+                                <Button onClick={handleRemoveConfirmationClose} color="primary">Cancel</Button>
+                            </DialogActions>
+                        </Dialog>
+                    </Fragment>)
+            }
         </Fragment>
     );
 }
