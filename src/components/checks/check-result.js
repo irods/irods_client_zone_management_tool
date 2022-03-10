bd7fbd1fa5b9fc3ff3f020c0d807c756e37b0e27
connectbo <zbo@renci.org>
2022-03-10 18:08:31 -0500

    [#118] made 'invalid' as status

--- src/components/checks/check-result.js
+++ src/components/checks/check-result.js
@@ -11,6 +11,7 @@ import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
 import HighlightOffIcon from '@material-ui/icons/HighlightOff'
 import EditIcon from '@material-ui/icons/Edit'
 import CloseIcon from '@material-ui/icons/Close';
+import CancelIcon from '@material-ui/icons/Cancel';
 import PropTypes from 'prop-types'
 import TimeAgo from 'timeago-react'
 
@@ -45,9 +46,9 @@ export const CheckResult = ({ status, check, result }) => {
                 <TableCell style={{ cursor: 'pointer' }} onClick={() => setExpanded(!expanded)}>{result[0].name}</TableCell>
                 <TableCell><i>{result[1].timestamp === 'N/A' ? 'N/A' : <TimeAgo datetime={result[1].timestamp} />}</i></TableCell>
                 <TableCell align="center"><input defaultChecked={!inactiveChecks.has(result[0].id)} disabled={!check.isValid} type="checkbox" onClick={() => modifyCheckActivity(result[0].id)} /></TableCell>
-                <TableCell align="center">{checkIntervals[check.id]} {checkIntervals[check.id]!== 'N/A' && 'seconds'}</TableCell>
+                <TableCell align="center">{checkIntervals[check.id]} {checkIntervals[check.id] !== 'N/A' && 'seconds'}</TableCell>
                 <TableCell align="center"><Tooltip title={inactiveChecks.has(result[0].id) ? "This check is inactive" : "Run this check again"}><span><IconButton size="small" disabled={inactiveChecks.has(result[0].id) || !check.isValid} onClick={() => runOneCheck(check)}><LoopIcon /></IconButton></span></Tooltip></TableCell>
-                <TableCell style={{ paddingLeft: '20px' }}>{status === 'healthy' ? <CheckIcon style={{ color: 'green' }} /> : (status === 'error' ? <ErrorIcon style={{ color: 'red' }} /> : (status === 'warning' ? <WarningIcon style={{ color: 'orange' }} /> : (status === 'unavailable' ? <HighlightOffIcon /> : <BlockIcon />)))}</TableCell>
+                <TableCell style={{ paddingLeft: '20px' }}>{status === 'healthy' ? <CheckIcon style={{ color: 'green' }} /> : (status === 'error' ? <ErrorIcon style={{ color: 'red' }} /> : (status === 'warning' ? <WarningIcon style={{ color: 'orange' }} /> : (status === 'unavailable' ? <HighlightOffIcon /> : (status === 'invalid' ? <CancelIcon style={{ color: 'red' }} /> : <BlockIcon />))))}</TableCell>
             </TableRow>
             <TableRow>
                 <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
