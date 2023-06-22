4ccd01521a20f8174b5b3e1f70066618a2b8ccb2
Ganning Xu <ganningxu@gmail.com>
2023-06-22 10:30:00 -0400

    [#111] added checkfile for storage resource children

---
+++ src/data/checkfiles/default_checkfiles/validate-storage-resc-children.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Link } from '@reach/router'
+
+export default {
+    name: "Storage resources have no children",
+    description: `Checks if all storage resources (unixfilesystem, univmss, s3) have no children`,
+    minimum_server_version: "4.2.0",
+    maximum_server_version: "",
+    interval_in_seconds: 86400,
+    active: true,
+    checker: function () {
+        let result = {
+            status: '',
+            message: '',
+            success: 0,
+            failed: []
+        }
+
+        const storageResources = ['unixfilesystem', 'univmss', 's3'];
+        const parentResourceIds = new Set(); // resources with children
+
+
+        this.rescAll._embedded.forEach(resc => {
+            const parent = resc[10];
+
+            if (parent) {
+                parentResourceIds.add(parent);
+            }
+        })
+
+        // loop through parentResourceIds and check if any are storage resources. if they are, add them to failed
+        parentResourceIds.forEach(id => {
+            const resc = this.rescAll._embedded.find(resc => resc[11] === id);
+            const name = resc[0];
+            const type = resc[1];
+            const zone = resc[2];
+
+            if (storageResources.includes(type)) {
+                result.failed.push([name, type, zone]);
+            }
+        })
+
+        result.status = result.failed.length > 0 ? 'error' : 'healthy'
+        result.message = result.failed.length > 0 ? <span><span>Failed on: </span>{result.failed.map((failedResc,index) => <span key={`rescNameCheckFailed-${index}`}>{index !== 0 && ', '}<Link className="check_result_link" to={`/resources?filter=${encodeURIComponent(failedResc[0])}`}>{failedResc[0]}:{failedResc[2]} ({failedResc[1]})</Link></span>)}</span> : 'All storage resources have no children.';
+
+        return result
+    }
+}
