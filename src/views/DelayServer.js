9221436abc93c2286fa417eaff2b4ae7cd74b816
Ganning Xu <ganningxu@gmail.com>
2023-06-26 18:25:26 -0400

    [141] got pre-population and custom hostname working

--- src/views/DelayServer.js
+++ src/views/DelayServer.js
@@ -1,7 +1,7 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { useEnvironment } from '../contexts';
-import { Button, TextField, Box, makeStyles, Snackbar, Chip, FormControl } from '@material-ui/core';
+import { useEnvironment, useServer } from '../contexts';
+import { Button, TextField, Box, makeStyles, Snackbar, Chip, LinearProgress, FormControl, Select } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
 
 const useStyles = makeStyles((theme) => ({
@@ -12,17 +12,34 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 export const DelayServer = () => {
-    const [hostname, setHostname] = useState('')
+    const noDelayServerValue = "nodelayserver.com"
+    const [hostname, setHostname] = useState(noDelayServerValue)
+    const [customHostname, setCustomHostname] = useState("")
     const [success, setSuccess] = useState(false)
     const [error, setError] = useState("")
+    const [customFieldOpen, setCustomFieldOpen] = useState(false)
     const environment = useEnvironment();
     const auth = localStorage.getItem('zmt-token')
     const classes = useStyles()
+    const { isLoadingZoneContext, zoneContext, filteredServers, loadCurrServers } = useServer();
+    
+
+    useEffect(() => {
+        loadCurrServers(0, 999999, "asc", "role");
+    }, []);
 
     const saveServerHostname = async () => {
-        if (hostname === "") {
-            setError("Hostname cannot be empty.")
-            return
+        let useHostname = "";
+
+        if (hostname === "custom_hostname" && customFieldOpen) {
+            if (customHostname === "") {
+                setError("Custom hostname cannot be empty. If you want to remove the delay server, select \"No Delay Server\".")
+                return;
+            }
+            useHostname = customHostname;
+            console.log('using custom hostname: ' + useHostname)
+        } else {
+            useHostname = hostname;
         }
 
         await axios({
@@ -33,7 +50,7 @@ export const DelayServer = () => {
             },
             params: {
                 'action': 'set_delay_server',
-                'target': hostname
+                'target': useHostname
             }
         }).then(() => {
             setSuccess(true)
@@ -43,15 +60,40 @@ export const DelayServer = () => {
 
 
     }
+
+    const getUseHostname = () => {
+        if (hostname === "custom_hostname" && customFieldOpen) {
+            return customHostname;
+        } else {
+            return hostname;
+        }
+    }
+
+    const handleSelectChange = (event) => {
+        console.log('setting hostname as: ' + event.target.value)
+        if (event.target.value === "custom_hostname") {
+            setCustomFieldOpen(true)
+        }
+        setHostname(event.target.value);
+    };
+
+    if (isLoadingZoneContext) {
+        return <LinearProgress />
+    }
     return (
         <Fragment>
-            <Box variant="h4" fontWeight="fontWeightBold" fontSize='h6.fontSize' >
-                <Box>
+          
+            <Box >
+                <Box variant="h4" fontWeight="fontWeightBold" fontSize='h6.fontSize'>
                     Change the delay server hostname (equivalent of <Chip label="iadmin set_delay_server" />):
                 </Box>
 
+                <Box marginY={'16px'}>
+                    Currently, the Zone Management Tool cannot show if you already have a delay server. This page is just to modify it.
+                </Box>
+
                 <Box display='flex' alignItems="center">
-                    <FormControl>
+                    {/* <FormControl>
                         <TextField
                         className={classes.inputBox}
                         id="delay_server"
@@ -64,6 +106,41 @@ export const DelayServer = () => {
                         value={hostname}
                         onChange={(e) => setHostname(e.target.value)}
                         />  
+                    </FormControl> */}
+
+                    <FormControl>
+                        <Select
+                            native
+                            onChange={handleSelectChange}
+                            value={hostname}
+                            >
+                                <option value={noDelayServerValue}>No Delay Server</option>
+                                {
+                                    filteredServers && filteredServers.map((server, index) => {
+                                        return <option value={server.host_system_information.hostname} key={index}>{server.host_system_information.hostname}</option>
+                                    })
+                                }
+
+                                <option value='custom_hostname'>Custom hostname</option>
+
+                                
+                            </Select>
+
+                            {
+                                customFieldOpen && <TextField
+                                className={classes.inputBox}
+                                id="delay_server"
+                                label="example.org"
+                                variant="outlined"
+                                margin="dense"
+                                required
+                                autoFocus
+                                size='medium'
+                                value={customHostname}
+                                onChange={(e) => setCustomHostname(e.target.value)}
+                                />
+                            }
+
                     </FormControl>
 
                     <Button variant="outlined" color="primary" onClick={saveServerHostname}>
@@ -73,10 +150,10 @@ export const DelayServer = () => {
             </Box>
 
 
-            <Snackbar open={success} autoHideDuration={5000} onClose={() => { setSuccess(false); }}><MuiAlert elevation={6} variant="filled" severity="success">Success! Delay server &quot;{hostname}&quot; was saved.</MuiAlert></Snackbar>
+            <Snackbar open={success} autoHideDuration={5000} onClose={() => { setSuccess(false); }}><MuiAlert elevation={6} variant="filled" severity="success">Success! Delay server &quot;{getUseHostname()}&quot; was saved.</MuiAlert></Snackbar>
             
 
-            <Snackbar open={error !== ""} autoHideDuration={5000} onClose={() => { setError(""); }}><MuiAlert elevation={6} variant="filled" severity="error">An error occured with saving &quot;{hostname}&quot;: {error}
+            <Snackbar open={error !== ""} autoHideDuration={5000} onClose={() => { setError(""); }}><MuiAlert elevation={6} variant="filled" severity="error">An error occured with saving &quot;{getUseHostname()}&quot;: {error}
             </MuiAlert></Snackbar>
            
 
