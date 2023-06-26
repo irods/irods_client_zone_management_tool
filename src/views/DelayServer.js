4306560efa2954063063646af48437f47c6a133c
Ganning Xu <ganningxu@gmail.com>
2023-06-26 14:23:44 -0400

    [141] added error checking

--- src/views/DelayServer.js
+++ src/views/DelayServer.js
@@ -1,17 +1,29 @@
 import React, { Fragment, useState } from 'react';
 import axios from 'axios';
 import { useEnvironment } from '../contexts';
-import { Button, TextField, Typography, Box, Snackbar, Chip } from '@material-ui/core';
+import { Button, TextField, Box, makeStyles, Snackbar, Chip, FormControl } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
 
-export const DelayServer = () => {
+const useStyles = makeStyles((theme) => ({
+    inputBox: {
+        marginRight: theme.spacing(1),
+        minWidth: 300
+    }
+}))
 
+export const DelayServer = () => {
     const [hostname, setHostname] = useState('')
     const [success, setSuccess] = useState(false)
+    const [error, setError] = useState("")
     const environment = useEnvironment();
     const auth = localStorage.getItem('zmt-token')
+    const classes = useStyles()
 
     const saveServerHostname = async () => {
+        if (hostname === "") {
+            setError("Hostname cannot be empty.")
+            return
+        }
 
         await axios({
             url: `${environment.restApiLocation}/admin`,
@@ -23,44 +35,36 @@ export const DelayServer = () => {
                 'action': 'set_delay_server',
                 'target': hostname
             }
-        }).then((res) => {
-            
-            console.log('res', res)
+        }).then(() => {
             setSuccess(true)
-            
         }).catch((e) => {
-            console.log('error', e)
+            setError(e.message)
         })
 
 
     }
     return (
         <Fragment>
-            <Typography variant="h4" gutterBottom>
-                Delay Server
-            </Typography>
             <Box variant="h4" fontWeight="fontWeightBold" fontSize='h6.fontSize' >
                 <Box>
-                    Set the delay server hostname (equivalent of <Chip label="iadmin set_delay_server" />):
+                    Change the delay server hostname (equivalent of <Chip label="iadmin set_delay_server" />):
                 </Box>
 
-                <Box display='flex' alignItems="center" >
-                    <TextField
-                    id="delay_server"
-                    label="Delay Server Hostname"
-                    variant="outlined"
-                    margin="dense"
-                    required
-                    autoFocus
-                    size='medium'
-                    width='50%'
-                    sx={{
-                        width: '300px',
-                        marginRight: 4
-                    }}
-                    value = {hostname}
-                    onChange = {(e) => setHostname(e.target.value)}
-                    />  
+                <Box display='flex' alignItems="center">
+                    <FormControl>
+                        <TextField
+                        className={classes.inputBox}
+                        id="delay_server"
+                        label="example.org"
+                        variant="outlined"
+                        margin="dense"
+                        required
+                        autoFocus
+                        size='medium'
+                        value={hostname}
+                        onChange={(e) => setHostname(e.target.value)}
+                        />  
+                    </FormControl>
 
                     <Button variant="contained" color="primary" onClick={saveServerHostname}>
                         Save
@@ -70,6 +74,10 @@ export const DelayServer = () => {
 
 
             <Snackbar open={success} autoHideDuration={5000} onClose={() => { setSuccess(false); }}><MuiAlert elevation={6} variant="filled" severity="success">Success! Delay server &quot;{hostname}&quot; was saved.</MuiAlert></Snackbar>
+            
+
+            <Snackbar open={error !== ""} autoHideDuration={5000} onClose={() => { setError(""); }}><MuiAlert elevation={6} variant="filled" severity="error">An error occured with saving &quot;{hostname}&quot;: {error}
+            </MuiAlert></Snackbar>
            
 
 
