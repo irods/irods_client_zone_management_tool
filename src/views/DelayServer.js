787e02879c39ecb22065a1713daf9c8ca6b6b737
Ganning Xu <ganningxu@gmail.com>
2023-06-26 13:19:12 -0400

    [141] got basic delay server modification working

--- src/views/DelayServer.js
+++ src/views/DelayServer.js
@@ -1,59 +1,78 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import { Link, navigate, useLocation } from '@reach/router';
+import React, { Fragment, useState } from 'react';
 import axios from 'axios';
-import { useEnvironment, useServer } from '../contexts';
-import { makeStyles, StylesProvider, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, LinearProgress, TextField, Typography, Input, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination, TableSortLabel, Select, Paper } from '@material-ui/core';
-import SaveIcon from '@material-ui/icons/Save';
-import CloseIcon from '@material-ui/icons/Close';
-import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
-
-const useStyles = makeStyles((theme) => ({
-    tableContainer: {
-        marginTop: 20
-    },
-    errorMsg: {
-        color: 'red'
-    },
-    link_button: {
-        textDecoration: 'none'
-    },
-    pagination: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    filterGroup: {
-        display: 'flex',
-        flexDirection: 'row',
-        margin: theme.spacing(1),
-        justifyContent: 'center'
-    },
-    filter: {
-        marginLeft: 30,
-        width: 300
-    },
-    add_user_name: {
-        width: 200
-    },
-    add_button: {
-        marginLeft: 30
-    },
-    fontInherit: {
-        font: 'inherit'
-    },
-    table_cell: {
-        wordWrap: 'break-word'
-    }
-}));
+import { useEnvironment } from '../contexts';
+import { Button, TextField, Typography, Box, Snackbar, Chip } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
 
 export const DelayServer = () => {
 
+    const [hostname, setHostname] = useState('')
+    const [success, setSuccess] = useState(false)
+    const environment = useEnvironment();
+    const auth = localStorage.getItem('zmt-token')
 
+    const saveServerHostname = async () => {
+
+        await axios({
+            url: `${environment.restApiLocation}/admin`,
+            method: 'POST',
+            headers: {
+                'Authorization': auth
+            },
+            params: {
+                'action': 'set_delay_server',
+                'target': hostname
+            }
+        }).then((res) => {
+            
+            console.log('res', res)
+            setSuccess(true)
+            
+        }).catch((e) => {
+            console.log('error', e)
+        })
+
+
+    }
     return (
         <Fragment>
             <Typography variant="h4" gutterBottom>
                 Delay Server
             </Typography>
+            <Box variant="h4" fontWeight="fontWeightBold" fontSize='h6.fontSize' >
+                <Box>
+                    Set the delay server hostname (equivalent of <Chip label="iadmin set_delay_server" />):
+                </Box>
+
+                <Box display='flex' alignItems="center" >
+                    <TextField
+                    id="delay_server"
+                    label="Delay Server Hostname"
+                    variant="outlined"
+                    margin="dense"
+                    required
+                    autoFocus
+                    size='medium'
+                    width='50%'
+                    sx={{
+                        width: '300px',
+                        marginRight: 4
+                    }}
+                    value = {hostname}
+                    onChange = {(e) => setHostname(e.target.value)}
+                    />  
+
+                    <Button variant="contained" color="primary" onClick={saveServerHostname}>
+                        Save
+                    </Button>
+                </Box>
+            </Box>
+
+
+            <Snackbar open={success} autoHideDuration={5000} onClose={() => { setSuccess(false); }}><MuiAlert elevation={6} variant="filled" severity="success">Success! Delay server &quot;{hostname}&quot; was saved.</MuiAlert></Snackbar>
+           
+
+
         </Fragment>
     );
 }
