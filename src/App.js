b92ce914cab53343b23ab453776ec67bd49b483c
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-09-17 15:30:12 -0400

    rearranging the components better.

--- src/App.js
+++ src/App.js
@@ -22,25 +22,26 @@ const App = () => {
                 <EnvironmentProvider>
                     <ServerProvider>
                         <CheckProvider>
-                        <ThemeProvider theme={theme}>
-                        <Layout>
-                            <Routes>
-                                <Route path="/" element={<Landing/>}/>
-                                <Route path="/home" element={ <Home timeStamp={new Date()} /> }/>
-                                <Route path='/users/edit' element={ <EditUser /> }/>
-                                <Route path="/users" element={ <User /> }/>
-                                <Route path='groups/edit' element={ <EditGroup /> }/>
-                                <Route path='/groups' element={ <Group /> }/>
-                                <Route path='/resources' element={ <ResourceListView /> }/>
-                                <Route path='/resources/tree' element={ <ResourceTreeView /> }/>
-                                <Route path='/servers' element={ <Server /> }/>
-                                <Route path='/specific-query' element={ <SpecificQuery /> }/>
-                                <Route path='/zones' element={ <Zone /> }/>
-                                <Route default element={ <NotFound /> }/>
-                            </Routes>
-                            </Layout>
+                            <ThemeProvider theme={theme}>
+                                <Layout>
+                                    <Routes>
+                                        <Route path="/" element={<Landing/>}/>
+                                        <Route path="/home" element={ <Home timeStamp={new Date()} /> }/>
+                                        <Route path='/users/edit' element={ <EditUser /> }/>
+                                        <Route path="/users" element={ <User /> }/>
+                                        <Route path='groups/edit' element={ <EditGroup /> }/>
+                                        <Route path='/groups' element={ <Group /> }/>
+                                        <Route path='/resources' element={ <ResourceListView /> }/>
+                                        <Route path='/resources/tree' element={ <ResourceTreeView /> }/>
+                                        <Route path='/servers' element={ <Server /> }/>
+                                        <Route path='/specific-query' element={ <SpecificQuery /> }/>
+                                        <Route path='/zones' element={ <Zone /> }/>
+                                        <Route default element={ <NotFound /> }/>
+                                    </Routes>
+                                </Layout>
+
+                                <Footer />
                             </ThemeProvider>
-                            <Footer />
                         </CheckProvider>
                     </ServerProvider>
                 </EnvironmentProvider>
